16b9c69735f55dd5b43c63deed501d4f2219b9b8
mich-murphy <github@elmurphy.com>
2026-06-09 12:50:27 +1000

    chore(terraform): centralize module provider versions

--- terraform/modules/proxmox_vm/versions.tf
+++ terraform/modules/proxmox_vm/versions.tf
@@ -2,12 +2,10 @@ terraform {
   required_version = ">= 1.6.0"
   required_providers {
     proxmox = {
-      source  = "bpg/proxmox"
-      version = "0.109.0"
+      source = "bpg/proxmox"
     }
     local = {
-      source  = "hashicorp/local"
-      version = "2.9.0"
+      source = "hashicorp/local"
     }
   }
 }
